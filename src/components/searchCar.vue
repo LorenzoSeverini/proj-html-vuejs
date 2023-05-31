c6a7fe9d2e9ae64c292ce0802f62003780df34d4
LorenzoSeverini <lorenzo.seve98@gmail.com>
2023-05-31 16:15:53 +0200

    fix search section

--- src/components/searchCar.vue
+++ src/components/searchCar.vue
@@ -65,8 +65,6 @@
                             { title: 'Used'},
                         ],
                     }, 
-
-                    { title: 'Search',}
                 ],
             }
         },
@@ -91,10 +89,10 @@
     <div class="container">
         <div class="container-caract">
             <div class="container-caract-item" v-for="car in autoCaracteristic" :key="car.title">
-                
                 <h1>{{ car.title }}</h1>
-               
+                <span class="fa-solid fa-caret-down fa-sm" style="color: #a6a6a6;"></span>
             </div>
+            <button class="btn-search-car">Search</button>
         </div>
     </div>
 
@@ -121,23 +119,44 @@
 
         .container-caract-item {
             width: 100%;
+            height: 75px;
             border: 1px solid $color-black;
             border-radius: 0.8rem;
-            padding: 3rem;
+            padding: 2rem;
             cursor: pointer;
-
-            &:hover {
-                background-color: $color-secondary;
-            }
+            transition: all 0.3s ease-in-out;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            text-align: center;
 
             h1 {
+                width: 100%;
+                height: 100%;
                 font-size: 1.5rem;
                 font-weight: 700;
                 color: $color-black;
+                display: flex;
+                justify-content: center;    
+                align-items: center;
+                text-align: center;
+            }
+        }
 
-                &:hover {
-                    color: $color-white;
-                }
+        .btn-search-car {
+            width: 100%;
+            border: 1px solid $color-secondary;
+            background-color: $color-secondary;
+            color: $color-white;
+            border-radius: 0.8rem;
+            padding: 3rem;
+            cursor: pointer;
+            transition: all 0.3s ease-in-out;
+
+            &:hover {
+                background-color: $color-tertiary;
+                border: 1px solid $color-tertiary;
+                color: $color-secondary;
             }
         }
     }
