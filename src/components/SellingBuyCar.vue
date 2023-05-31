4727175bed1eba5fb20ca901765b490e2a3764bc
LorenzoSeverini <lorenzo.seve98@gmail.com>
2023-05-31 14:17:17 +0200

    fix and add conatiner right content

--- src/components/SellingBuyCar.vue
+++ src/components/SellingBuyCar.vue
@@ -1,6 +1,5 @@
 <!-- Buy or selling? -->
 <script>
-
     export default {
         name: 'SellingBuyCar',
 
@@ -26,8 +25,9 @@
 
     <div class="container">
         <div class="container-left">
-            <img :src="left.image" alt="about car">
-            
+            <img src="../assets/AutoCar Projcet Asset/imgs/assets/aoutcar-about-1.png" alt="about car">
+            <div class="circle-1">24<br> Years of experince</div>
+            <div class="circle-2">240<br> special export Team</div>
         </div>
         <div class="container-right">
             <div class="title">Want to Buy or sell a vehicle?</div>
@@ -39,8 +39,22 @@
                 vehicles quickly.
             </div>
             <img src="../assets/AutoCar Projcet Asset/imgs/assets/divider.jpg" alt="divider">
+            <div class="container-vehicles">
+                <img src="../assets/AutoCar Projcet Asset/imgs/assets/free-fa-brands-400.svg-fonta.svg" alt="">
+                <div class="text">
+                    <h3>Model vehicles</h3>
+                    <p>We are sure that you will find vehicles of suitable models on our website.</p>
+                </div>
+            </div>
+            <div class="container-vehicles">
+                <img src="../assets/AutoCar Projcet Asset/imgs/assets/free-fa-v4deprecations.svg-fonta.svg" alt="">
+                <div class="text">
+                    <h3>2nd Hand Vehicles</h3>
+                    <p>You can put your used vehicles for sale by adding them to our website.</p>
+                </div>
+            </div>
             <div class="button-us">
-                <button>Show All Cars</button>
+                <button>Get to know us <span class="fa-solid fa-arrow-right"></span></button>
             </div>
 
         </div>
@@ -56,19 +70,57 @@
 
 .container {
     max-width: 140rem;
-    margin: 3.125rem auto;
+    margin: 10.125rem auto;
     display: flex;
-    min-height: 1000px;
 
     .container-left {
         width: 50%;
         background-image: url('../assets/AutoCar Projcet Asset/imgs/assets/circle-auto-car-1.png');
         background-repeat: no-repeat;
         text-align: center;
+        position: relative;
+        margin-right: 6.25rem;
 
         img {
+            object-fit: cover;
             width: 100%;
-            height: 100%;
+            object-position: center;
+        }
+
+        .circle-1 {
+            width: 18rem;
+            height: 18rem;
+            background-color: $color-secondary;
+            border-radius: 50%;
+            color: $color-white;
+            font-size: 3rem;
+            font-weight: 700;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            margin: 2rem auto;
+            position: absolute;
+            top: 10%;
+            left: 10%;
+            transform: translate(-5%, -20%);
+        }
+
+        .circle-2 {
+            width: 18rem;
+            height: 18rem;
+            background-color: $color-secondary;
+            border-radius: 50%;
+            color: $color-white;
+            font-size: 2.5rem;
+            font-weight: 700;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            margin: 2rem auto;
+            position: absolute;
+            top: 90%;
+            right: 2%;
+            transform: translate(-3%, -100%);
         }
     }
 
@@ -100,6 +152,41 @@
             margin-bottom: 1.875rem;
         }
 
+        .container-vehicles {
+            display: flex;
+            height: 10rem;
+
+            img {
+                width: 10%;
+                height: 30%;
+                margin-right: 1.875rem;
+                fill: rgb(184, 167, 167);
+                color: rgb(204, 75, 75);
+                background-color: black;
+                display: flex;
+                justify-content: center;
+                align-items: center;
+            }
+
+            .text {
+
+                h3 {
+                    font-size: 1.875rem;
+                    font-weight: 700;
+                    color: $color-black;
+                    margin-bottom: 1.875rem;
+                }
+
+                p {
+                    font-size: 1.5rem;
+                    font-weight: 400;
+                    color: $color-black;
+                    margin-bottom: 1.875rem;
+                }
+            }
+            
+        }
+
         .button-us {
             display: flex;
             margin-bottom: 1.875rem;
@@ -119,10 +206,7 @@
                     background-color: $color-tertiary;
                 }
             }
-}
-
-        
-
+        }
     }
 }
 </style>
