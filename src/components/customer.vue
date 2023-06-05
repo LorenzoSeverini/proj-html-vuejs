4996df5c7291f253ef8134b298431f6face1e9ee
LorenzoSeverini <lorenzo.seve98@gmail.com>
2023-06-05 22:52:21 +0200

    add content for show more

--- src/components/customer.vue
+++ src/components/customer.vue
@@ -29,6 +29,45 @@
                     },  
                 ],
             },
+
+            customer2: {
+                type: Array,
+                default: () => [
+                    { 
+                        text:'We are a gallery vehicle dealer. Before we met this site. our sales were sluggish.', 
+                        image:'/src/assets/AutoCar Projcet Asset/imgs/assets/man1.png', 
+                        name:'Adam',
+                    },
+                    {
+                        text:'One of the quality websites I apply to rent a car. There are hundreds of different tools.',
+                        image:'/src/assets/AutoCar Projcet Asset/imgs/assets/man2.png',
+                        name:'Emma',
+                    },
+                    {
+                        text:'I was trying to sell my car. And I put it here and sold it for more than it was worth.',
+                        image:'/src/assets/AutoCar Projcet Asset/imgs/assets/testimonial2-1.png',
+                        name:'John',
+                    },
+                    {
+                        text:'A fast and reliable vehicle selling site. Rare quality website with hundreds of model vehicles.',
+                        image:'/src/assets/AutoCar Projcet Asset/imgs/assets/testimonial4-1.png',
+                        name:'John',   
+                    },  
+                ]
+            },
+        },
+
+        data() {
+            return {
+                showFirstArray: true,
+            };
+        },
+
+
+        created() {
+            setInterval(() => {
+            this.showFirstArray = !this.showFirstArray;
+            }, 5000);
         },
     }
 </script>
@@ -51,7 +90,8 @@
         </div>
 
         <div class="container-card">
-            <div class="customer">
+            <!-- 1 -->
+            <div class="customer" v-show="showFirstArray">
                 <div class="customer-card" v-for="item in customer" :key="item.id">
                     <div class="customer-card-text">{{ item.text }}</div>
                     <div class="customer-card-image">
@@ -60,7 +100,25 @@
                     <div class="customer-card-name">{{ item.name }}</div>
                 </div>
             </div>
-        </div>  
+            <!-- 2 -->
+            <div class="customer2" v-show="!showFirstArray">
+                <div class="customer-card" v-for="item in customer2" :key="item.id">
+                    <div class="customer-card-text">{{ item.text }}</div>
+                    <div class="customer-card-image">
+                        <img :src="item.image" alt="customer">
+                    </div>
+                    <div class="customer-card-name">{{ item.name }}</div>
+                </div>
+            </div>
+        </div>
+
+        <!-- Buttons -->
+        <div class="slide-buttons-cont">
+            <!-- first -->
+            <button @click="showFirstArray = true" class="slide-button1"></button>
+            <!-- second -->
+            <button @click="showFirstArray = false" class="slide-button2"></button>
+        </div>
     </div>
 
 </template>
@@ -69,7 +127,6 @@
 <style lang="scss" scoped>
 
 @use '../styles/partials/_variables' as *;
-@use '../styles/partials/_mixing' as *;
 
 .container {
     background-image: url('../assets/AutoCar Projcet Asset/imgs/assets/comment-bg-autocar.jpg');
@@ -114,7 +171,7 @@
         max-width: 140rem;
         margin: 3.125rem auto;
 
-        .customer {
+        .customer, .customer2 {
             display: flex;
             justify-content: center;
             align-items: center;
@@ -163,6 +220,48 @@
                     font-weight: 700;
                     color: $color-black;
                 }
+            }
+        }
+
+        .customer2 .customer-card {
+            background-color: $color-secondary;
+        }
+
+    }
+
+    .slide-buttons-cont {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        gap: 2rem;
+        
+        .slide-button1 {
+            width: 10rem;
+            height: 1.5rem;
+            background-color: $color-primary;
+            border: 1px solid $color-black;
+            border-radius: .625rem;
+            transition: background-color 3s ease-in-out;
+            cursor: pointer;
+
+            &:hover {
+                background-color: $color-secondary;
+                transition: background-color 0.3s ease-in-out;
+            }
+        }
+
+        .slide-button2 {
+            width: 5rem;
+            height: 1.5rem;
+            background-color: $color-secondary;
+            border: 1px solid $color-black;
+            border-radius: .625rem;
+            transition: background-color 3s ease-in-out;
+            cursor: pointer;
+
+            &:hover {
+                background-color: $color-primary;
+                transition: background-color 0.3s ease-in-out;
             }
         }
     }
